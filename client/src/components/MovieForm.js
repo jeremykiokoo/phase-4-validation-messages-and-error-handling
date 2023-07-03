bbb2c5578646416738f175fbf0ca4b8ffa3f02ba
jeremykioko <jeremy.kioko@student.moringaschool.com>
2023-07-03 10:50:27 +0300

    complete

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -14,6 +14,27 @@ function MovieForm() {
     female_director: false,
   });
 
+  const [errors, setErrors] = useState([]);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    // fetch returns a Promise, we must await it
+    const response = await fetch("/movies", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    // response.json() returns a Promise, we must await it
+    const data = await response.json();
+    if (response.ok) {
+      console.log("Movie created:", data);
+    } else {
+      setErrors(data.errors);
+    }
+  }
+/*
   function handleSubmit(e) {
     e.preventDefault();
     fetch("/movies", {
@@ -23,10 +44,19 @@ function MovieForm() {
       },
       body: JSON.stringify(formData),
     })
+    .then((response) => {
+      if (response.ok) {
+        response.json().then((newMovie) => console.log(newMovie));
+      } else {
+        response.json().then((errorData) => setErrors(errorData.errors));
+      }
+    })
+    
+      /*then ((response) => console.log(response));
       .then((response) => response.json())
       .then((newMovie) => console.log(newMovie));
   }
-
+*/
   function handleChange(e) {
     const value =
       e.target.type === "checkbox" ? e.target.checked : e.target.value;
@@ -127,6 +157,16 @@ function MovieForm() {
         </FormGroup>
         <SubmitButton type="submit">Add Movie</SubmitButton>
       </form>
+      {
+      errors.length > 0 && (
+        <ul style={{ color: "red" }}>
+          {errors.map((error) => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
+  )
+}
+
     </Wrapper>
   );
 }
